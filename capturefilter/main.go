3c2531877b79928c88ef76116f669ae020fb1763
Jesse Lesperance <jesse@jplesperance.me>
2018-03-14 15:59:01 -0700

    added filtered packet capture

---
+++ capturefilter/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"log"
+	"time"
+)
+
+var (
+	device            = "eth0"
+	snapshotLen int32 = 1024
+	promiscuous       = false
+	err         error
+	timeout     = 30 * time.Second
+	handle      *pcap.Handle
+)
+
+func main() {
+	handle, err := pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	var filter string = "tcp and port 80"
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Only capturing TCP port 80 packets")
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		fmt.Println(packet)
+	}
+}
