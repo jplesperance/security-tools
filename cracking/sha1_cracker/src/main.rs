7fb1e5e581c78d73011215759ef235135426f0f2
Jesse Lesperance <jesse@lesperance.io>
2023-03-30 23:53:56 -0700

    adding basic hash length checking

--- cracking/sha1_cracker/src/main.rs
+++ cracking/sha1_cracker/src/main.rs
@@ -1,12 +1,24 @@
-use std::env;
+use std::{
+    env,
+    error::Error,
+};
 
-fn main() {
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>> 0{
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
         println!("Usage: ");
         println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
-        return;
+        return Ok(());
     }
+
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());
+    }
+
+    Ok(())
 
 }
