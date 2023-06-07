fc0223210bfbe2230bd1357ab93cf2d3ab86e8b9
Jesse P Lesperance <jesse@jplesperance.me>
2023-06-06 18:01:25 -0700

    cleaned up code

--- cracking/sha256_cracker/src/main.rs
+++ cracking/sha256_cracker/src/main.rs
@@ -3,8 +3,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use sha2::{Sha256, Digest};
 use std::process::exit;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
@@ -13,28 +14,29 @@ fn main() {
         exit(1);
     }
 
-    let wanted_hash: &String = &args[1];
-    let password_file: &String = &args[2];
+    let wanted_hash = &args[1];
+    let password_file = &args[2];
     let mut attempts = 1;
 
     println!("Attempting to crack: {}!\n", wanted_hash);
 
-    let password_list: File = File::open(password_file).unwrap();
-    let reader: BufReader<File> = BufReader::new(password_list);
+    let password_list = File::open(password_file)?;
+    let reader = BufReader::new(password_list);
 
     for line in reader.lines() {
-        let line: String = line.unwrap();
-        let password: Vec<u8> = line.trim().to_owned().into_bytes();
-        let password_hash: String = format!("{:x}", Sha256::digest(&password));
+        let line = line?;
+        let password = line.trim().as_bytes();
+        let password_hash = format!("{:x}", Sha256::digest(password));
 
-        println!("[{}] {} == {}", attempts, std::str::from_utf8(&password).unwrap(), password_hash);
-        if &password_hash == wanted_hash {
+        println!("[{}] {} == {}", attempts, std::str::from_utf8(password)?, password_hash);
+        if password_hash == *wanted_hash {
             println!("Password Found:");
-            println!("Password: {} found after {} attempts!!", std::str::from_utf8(&password).unwrap(), attempts);
+            println!("Password: {} found after {} attempts!!", std::str::from_utf8(password)?, attempts);
             exit(0);
         }
         attempts +=1;
     }
 
-    println!("Not matches found!")
+    println!("Not matches found!");
+    Ok(())
 }
